e3203ddd1d387c0e7bacf0133fb0cd399c9942fb
yhcheng3 <yuihangc127@gmail.com>
2024-08-20 08:40:28 +0800

    PID test.

--- USER/main.c
+++ USER/main.c
@@ -19,8 +19,19 @@ int main(void)
   int ECPULSE1 = 0,ECPULSE2 = 0,ECPULSE3 = 0;
 	char txt_pulse[30];
 	
-	    int16_t duty = 1000,flag = 0;
-    char txt[16];
+	int16_t duty = 1000,flag = 0;
+  char txt[16];
+	
+	int target = 80;
+	float output = 0;
+	
+//----PID----
+	pid_param_t pid;
+	PidInit(&pid);
+	pid.kd = 10;
+	pid.ki = 10;
+	pid.kp = 10;
+	
 	
 //-----------------------系统初始化配置----------------------------
 	HAL_Init();			  // 初始化HAL库
@@ -36,39 +47,81 @@ int main(void)
     OLED_Show_LQLogo(); // 显示LOGO
 	delay_ms(500);		// 延时等待
 	OLED_CLS();			// 清屏
-	OLED_P8x16Str(10, 0,"Motor_Enc Test");       //显示字符串
+	MotorInit();
+	// OLED_P8x16Str(10, 0,"Motor_Enc Test");       //显示字符串
 	while(1)
 	{
 		ECPULSE1=Read_Encoder(2);
 		sprintf(txt_pulse,"E1:%04d ",ECPULSE1);
-	    OLED_P8x16Str(0,2,txt_pulse);	
-		ECPULSE2=Read_Encoder(3);
-		sprintf(txt_pulse,"E2:%04d ",ECPULSE2);
-	    OLED_P8x16Str(0,4,txt_pulse);	
-		ECPULSE3=Read_Encoder(4);
-		sprintf(txt_pulse,"E3:%04d ",ECPULSE3);
-	    OLED_P8x16Str(0,6,txt_pulse);	
+	    OLED_P8x16Str(0,0,txt_pulse);	
+		
+		output = PidLocCtrl(&pid, (ECPULSE1 * 10) - duty);
+//		ECPULSE2=Read_Encoder(3);
+//		sprintf(txt_pulse,"E2:%04d ",ECPULSE2);
+//	    OLED_P8x16Str(0,4,txt_pulse);	
+		
+//		ECPULSE3=Read_Encoder(4);
+//		sprintf(txt_pulse,"E3:%04d ",ECPULSE3);
+//	    OLED_P8x16Str(0,6,txt_pulse);	
     
-		if(Read_key(KEY1) == 1)      //按下KEY0键，占空比减小
+//		if(Read_key(KEY1) == 1)      //按下KEY0键，占空比减小
+//    {
+//      if(duty > -PWM_DUTY_MAX)
+//        duty += 200;
+//    }
+//    if(Read_key(KEY2) == 1)      //按下KEY2键，占空比加大
+//    {
+//      if(duty < PWM_DUTY_MAX)   //满占空比为10000，限制7200
+//        duty = 0;
+//    }
+//    if(Read_key(KEY3) == 1)      //按下KEY1键，占空比中值
+//    {
+//      duty -= 200;
+//    }
+		
+    if(Read_key(KEY1) == 1)      //按下KEY2键，占空比加大
     {
-      if(duty > -PWM_DUTY_MAX)
-        duty += 200;
+      pid.kp += 1;
     }
-    if(Read_key(KEY2) == 1)      //按下KEY2键，占空比加大
+		if(Read_key(KEY2) == 1)      //按下KEY2键，占空比加大
     {
-      if(duty < PWM_DUTY_MAX)   //满占空比为10000，限制7200
-        duty = 0;
+      pid.kp += 1;
     }
-    if(Read_key(KEY3) == 1)      //按下KEY1键，占空比中值
+		if(Read_key(KEY3) == 1)      //按下KEY2键，占空比加大
     {
-      duty -= 200;
+      pid.kp += 1;
+    }
+		if(Read_key(KEY3) == 1)      //按下KEY2键，占空比加大
+    {
+      pid.kp += 1;
     }
 		
-		MotorCtrl3w(duty,duty, duty);
-    sprintf(txt, "PWM: %5d;", duty);
-		OLED_P8x16Str(20,5,txt);
+		if(Read_key(KEY4) == 1)      //按下KEY2键，占空比加大
+    {
+      pid.ki += 1;
+    }
 		
-        delay_ms(100);   //编码器计数累计时间，时间越大 读取值越大
+		if(Read_key(KEY5) == 1)      //按下KEY2键，占空比加大
+    {
+      pid.kd += 1;
+    }
+		sprintf(txt_pulse,"E2:%04f ",pid.kp);
+	    OLED_P8x16Str(0,2,txt_pulse);	
+		sprintf(txt_pulse,"E2:%04f ",pid.ki);
+	    OLED_P8x16Str(0,4,txt_pulse);	
+		sprintf(txt_pulse,"E2:%04f ",pid.kd);
+	    OLED_P8x16Str(0,6,txt_pulse);	
+		
+		
+		output = constrain_float(output, -2000, 2000);
+		
+		MotorCtrl3w(output, 0, 0);
+		// MotorCtrl3w(duty, duty, duty);
+    // sprintf(txt, "PWM: %5d;", duty);
+		// OLED_P8x16Str(10,0,txt);
+		
+		LED_Ctrl(RVS);       //电平翻转,LED闪烁
+    delay_ms(200);   //编码器计数累计时间，时间越大 读取值越大
 		
 	}
 }
